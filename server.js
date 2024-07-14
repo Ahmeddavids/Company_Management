2e86b4af9273d60eeb430015191ffb1988f7dedc
ahmed isiaka <ahmeddavids6@gmail.com>
2024-07-14 05:49:55 +0100

    Updated with task and employees routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const express = require('express');
 const PORT = process.env.PORT || 2021;
 const userRouter = require('./routers/userRouter');
+const workerRouter = require('./routers/workerRouter');
+const taskRouter = require('./routers/taskRouter');
 
 const app = express();
 app.use(cors({origin: "*"}));
@@ -12,6 +14,8 @@ app.use(morgan("dev"))
 app.use(express.json());
 
 app.use('/api', userRouter)
+app.use('/api/employee', workerRouter)
+app.use('/api/task', taskRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening to PORT: ${PORT}`);
